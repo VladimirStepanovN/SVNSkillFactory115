4e3d880d8c1d02ca70ef08f3f7a05304fbad6d52
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-08-17 00:07:26 +0500

    Решение к 15.6.6. Практическое задание (HW-03).

---
+++ Classroom.cs
@@ -0,0 +1,7 @@
+﻿namespace SVNSkillFactory115
+{
+    public class Classroom
+    {
+        public List<string> Students = new();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace SVNSkillFactory115
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = from classRoom in classes
+                         from student in classRoom.Students
+                         select student;
+            return result.ToArray();
         }
     }
 }
